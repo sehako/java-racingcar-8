b14cf19386c3264f282b8879ff0e1ce46ead14db
sehako <dhtpgkr1999@gmail.com>
2025-10-25 20:19:03 +0900

    feat(input): 시도 횟수 입력 안내 메시지 출력 및 입력 기능 구현

--- src/main/java/racingcar/RacingSimulator.java
+++ src/main/java/racingcar/RacingSimulator.java
@@ -3,6 +3,7 @@ package racingcar;
 public class RacingSimulator {
 
     private static final String CAR_NAMES_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String RACING_ATTEMPT_INPUT_GUIDE = "시도할 횟수는 몇 회인가요?";
 
     private final ConsoleReader consoleReader;
     private final ConsoleWriter consoleWriter;
@@ -15,5 +16,7 @@ public class RacingSimulator {
     public void simulate() {
         consoleWriter.printLine(CAR_NAMES_INPUT_GUIDE);
         String carNames = consoleReader.readLine();
+        consoleWriter.printLine(RACING_ATTEMPT_INPUT_GUIDE);
+        String racingAttempt = consoleReader.readLine();
     }
 }
