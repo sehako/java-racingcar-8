4453280cb6bbe4e91be6a1cd2a68263d48113bb3
sehako <dhtpgkr1999@gmail.com>
2025-10-27 00:37:10 +0900

    feat(racing): 경기 결과 출력 기능 구현

--- src/main/java/racingcar/RacingSimulatorController.java
+++ src/main/java/racingcar/RacingSimulatorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class RacingSimulatorController {
     private static final String CAR_NAMES_INPUT_GUIDE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String RACING_ATTEMPT_INPUT_GUIDE = "시도할 횟수는 몇 회인가요?";
+    private static final String RACING_RESULT_FORMAT = "최종 우승자 : %s";
 
     private final ConsoleWriter consoleWriter;
     private final ConsoleReader consoleReader;
@@ -29,7 +30,7 @@ public class RacingSimulatorController {
         int racingAttemptNumber = AttemptParser.parse(racingAttempt);
 
         List<String> winners = startRacing(racingCars, racingAttemptNumber);
-
+        printWinners(winners);
     }
 
     private List<String> startRacing(List<RacingCar> racingCars, int racingAttemptNumber) {
@@ -37,6 +38,11 @@ public class RacingSimulatorController {
                 .racingSimulator(consoleWriter, racingCars, racingAttemptNumber);
 
         return racingSimulator.simulate();
+    }
+
+    private void printWinners(List<String> winners) {
+        String winnerString = String.join(", ", winners);
+        consoleWriter.printLine(String.format(RACING_RESULT_FORMAT, winnerString));
     }
 
     public void stop() {
